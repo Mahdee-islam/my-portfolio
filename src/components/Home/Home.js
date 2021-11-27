6fde0c7cf345947892ca75016ff17aafed845139
Mahdi <hirelingmahdi@gmail.com>
2021-11-27 21:25:50 +0600

    updated homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,47 @@
 
 import React from 'react';
 import Typical from 'react-typical';
+import image from '../../profile.svg';
 import './Home.css';
 
+
+
 const Home = () => {
+    
     return (
-        <div className = "home">
+        <div>
+             <div className = "home">
              <div className="content-section">
-           <p> WELCOME TO MY <b className = "name">PORTFOLIO</b></p>
-       <h1>Hi, I'm <span className = "name">Mahdee Islam</span> 
+           <p> WELCOME TO MY <b >PORTFOLIO</b></p>
+       <h1>Hi, I'm <span className = "name"> <b>Mahdee</b> </span>Islam 
        {' '} 
-       <Typical loop = {Infinity}
+       <Typical className = "name" loop = {Infinity}
         wrapper = 'h1'
         steps = {
             [
-                'developer.',
+                'a web developer.',
                 1000,
-                'designer.',
+                'a self learner.',
                 1000,
-                'open sorcer.',
+                'a web designer.',
                 1000,
-                'freelancer.',
+                'a freelancer.',
                 1000,
             ]
         }
         />
-       
         </h1>
+        <p>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways that.</p>
         
             </div> 
             <div className="img-section">
-               
+                <img  src= { image } alt="img" /> 
             </div>
+             </div>
+             <p>Find Section</p>
+             <div className="find-section">
+              
+             </div>
         </div>
     );
 };
